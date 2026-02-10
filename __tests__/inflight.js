10bf000b88f4e5ac456e5ad881890aeefd97aeb6
Lily Skye <git@suchipi.tv>
2026-02-10 02:29:13 -0700

    add test for allInflightRunContexts

---
+++ __tests__/inflight.js
@@ -0,0 +1,34 @@
+const { spawn, allInflightRunContexts } = require("../src");
+
+test("one run", async () => {
+  const run1 = spawn("sleep", ["1"]);
+  expect(allInflightRunContexts).toContain(run1);
+  await run1.completion;
+  expect(allInflightRunContexts).not.toContain(run1);
+});
+
+test("multiple runs", async () => {
+  const run1 = spawn("sleep", ["0.2"]);
+  const run2 = spawn("sleep", ["0.4"]);
+  const run3 = spawn("sleep", ["0.6"]);
+  expect(allInflightRunContexts).toContain(run1);
+  expect(allInflightRunContexts).toContain(run2);
+  expect(allInflightRunContexts).toContain(run3);
+
+  run3.kill();
+  // wait for kill to go through...
+  await new Promise((resolve) => setTimeout(resolve, 5));
+  expect(allInflightRunContexts).toContain(run1);
+  expect(allInflightRunContexts).toContain(run2);
+  expect(allInflightRunContexts).not.toContain(run3);
+
+  await run1.completion;
+  expect(allInflightRunContexts).not.toContain(run1);
+  expect(allInflightRunContexts).toContain(run2);
+  expect(allInflightRunContexts).not.toContain(run3);
+
+  await run2.completion;
+  expect(allInflightRunContexts).not.toContain(run1);
+  expect(allInflightRunContexts).not.toContain(run2);
+  expect(allInflightRunContexts).not.toContain(run3);
+});
